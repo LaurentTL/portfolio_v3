1f02a4d261944a3f8eaf0b959fae921b7492c67b
Laurent Treuil <laurent.treuil@gmail.com>
2022-07-03 14:59:09 +0200

    iphone mokup

--- components/Works.jsx
+++ components/Works.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { ProjectBackground, ProjectWrapper, ProjectContent, Project } from './Project'
 import { WorkBackground, WorkContainer, WorkLeft, WorkRight, WorkLink } from './work'
 import rockPic from './../public/1.jpeg'
-import skillex from './../public/skillex.jpeg'
+import skillex from './../public/skillex.png'
 
 function Works() {
     return (
@@ -18,7 +18,7 @@ function Works() {
                             <WorkLeft progress={progress}>
                                 <div>I{`'`}ve build</div>
                                 <div className="text-4xl md:text-5xl font-semibold tracking-tight">
-                                    <WorkLink href={'https://skillexclone.netlify.app'}>Skillex home page integration</WorkLink>
+                                    <WorkLink href={'https://skillexclone.netlify.app'}>Skillex<br /> home page<br /> integration</WorkLink>
                                 </div>
                             </WorkLeft>
                             <WorkRight progress={progress}>
